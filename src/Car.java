f7efbb5098741a8a418abfe707eba39cb2656a51
violoniste <violoniste2@gmail.com>
2023-09-23 14:40:14 +0700

    Готово

---
+++ src/Car.java
@@ -0,0 +1,10 @@
+public class Car extends Vehicle {
+
+    public Car(String modelName, int wheelsCount) {
+        super(modelName, wheelsCount);
+    }
+
+    public void checkEngine() {
+        System.out.println("Проверяем двигатель");
+    }
+}
---
+++ src/Vehicle.java
@@ -0,0 +1,21 @@
+public class Vehicle {
+    private final String modelName;     // Будем считать, что эти вещи у транспорта не меняются
+    private final int wheelsCount;
+
+    public Vehicle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public int getWheelsCount() {
+        return wheelsCount;
+    }
+
+    public void updateTyre() {
+        System.out.println("Меняем покрышку");
+    }
+}
